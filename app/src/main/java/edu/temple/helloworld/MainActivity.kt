2294cc96b29ccae59ad988a58b4974c083d4d378
Timothy Bui <74019106+tun38646@users.noreply.github.com>
2023-09-25 15:38:45 -0400

    Declared and initialized all of the view properties from activity_main.xml inside of MainActivity.kt

--- app/src/main/java/edu/temple/helloworld/MainActivity.kt
+++ app/src/main/java/edu/temple/helloworld/MainActivity.kt
@@ -10,6 +10,8 @@ class MainActivity : AppCompatActivity() {
 
     // Declare view properties - the first one is done for you
     lateinit var displayTextView: TextView
+    lateinit var clickMeButton: Button
+    lateinit var nameEditText: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,8 +19,10 @@ class MainActivity : AppCompatActivity() {
 
         // Initialize with views defined in Layout - the first one is done for you
         displayTextView = findViewById(R.id.displayTextView)
+        clickMeButton = findViewById(R.id.clickMeButton)
+        nameEditText = findViewById(R.id.nameEditText)
 
-        
+
         findViewById<Button>(R.id.clickMeButton).setOnClickListener {
             displayTextView.text = "Hello, ${findViewById<EditText>(R.id.nameEditText).text}"
         }
